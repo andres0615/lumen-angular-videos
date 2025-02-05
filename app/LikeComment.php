975735c9c455d03a0825b86749ba10608a3ce90c
bafdurango <bafdurango@hotmail.com>
2025-02-04 21:59:22 -0500

    fix: Se maneja error al contar likes de comentarios desde el frontend

--- app/LikeComment.php
+++ app/LikeComment.php
@@ -29,102 +29,28 @@ class LikeComment extends Model
 
     public function getCommentLikes($commentId)
     {
-
-        $commentLikes = 0;
-        $maxAttempts = 2;
-        $intervalSeconds = 1;
-
-        for ($i=0; $i < $maxAttempts; $i++) {
-            $result = $this->countLikes($commentId);
-
-            $resultType = $result['type'];
-            $resultData = $result['data'];
-
-            if ($resultType === 'success') {
-                $commentLikes = $resultData['commentLikes'];
-                break;
-            } else {
-                sleep($intervalSeconds);
-            }
-        }
+        $commentLikes = LikeComment::where('comment_id', $commentId)
+                        ->where('type', true)
+                        ->count();
 
         return $commentLikes;
     }
 
-    public function countLikes($commentId)
-    {
-        $result = [];
-        $resultData = [];
-
-        // Log::info('=============== wzzsi22icidgdgr ==============');
-
-        try {
-            $commentLikes = LikeComment::where('comment_id', $commentId)
-                        ->where('type', true)
-                        ->count();
-
-            $resultData['commentLikes'] = $commentLikes;
-            $resultType = 'success';
-
-        } catch (Throwable $th) {
-            //throw $th;
-            $resultType = 'error';
-        }
-
-        $result['type'] = $resultType;
-        $result['data'] = $resultData;
-
-        return $result;
-    }
+    // public function countLikes($commentId)
+    // {
+    // }
 
     public function getCommentDislikes($commentId)
     {
-
-        $commentDislikes = 0;
-        $maxAttempts = 2;
-        $intervalSeconds = 1;
-
-        for ($i=0; $i < $maxAttempts; $i++) {
-            $result = $this->countDislikes($commentId);
-
-            $resultType = $result['type'];
-            $resultData = $result['data'];
-
-            if ($resultType === 'success') {
-                $commentDislikes = $resultData['commentDislikes'];
-                break;
-            } else {
-                sleep($intervalSeconds);
-            }
-        }
+        $commentDislikes = LikeComment::where('comment_id', $commentId)
+                        ->where('type', false)
+                        ->count();
 
         return $commentDislikes;
     }
 
-    public function countDislikes($commentId)
-    {
-        $result = [];
-        $resultData = [];
-
-        // Log::info('=============== j486ptbz3q9k8gr ==============');
-
-        try {
-            $commentDislikes = LikeComment::where('comment_id', $commentId)
-                        ->where('type', false)
-                        ->count();
-
-            $resultData['commentDislikes'] = $commentDislikes;
-            $resultType = 'success';
-
-        } catch (Throwable $th) {
-            //throw $th;
-            $resultType = 'error';
-        }
-
-        $result['type'] = $resultType;
-        $result['data'] = $resultData;
-
-        return $result;
-    }
+    // public function countDislikes($commentId)
+    // {
+    // }
 
 }
